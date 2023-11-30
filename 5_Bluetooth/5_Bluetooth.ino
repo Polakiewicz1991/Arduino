064629092ea43b8e1e91e2fb1f2cc678da122419
Polakiewicz1991 <polakiewicz1991@gmail.com>
2023-11-30 15:57:28 +0100

    Testy komunikacji

--- 5_Bluetooth/5_Bluetooth.ino
+++ 5_Bluetooth/5_Bluetooth.ino
@@ -4,6 +4,9 @@
 #include <SoftwareSerial.h>
 #include <PpBluetooth.h>
 
+#define SERIAL_RX_BUFFER_SIZE 256
+#define SERIAL_TX_BUFFER_SIZE 256
+
 //Hardware controll - cycle time
 const int iCycleTime = 100;
 
@@ -27,7 +30,8 @@ const int servoPin = 9;
 int iServoPos = 90;
 
 //Hardware Bluetooth module - Initialization
-PpBluetooth BtDevice(9600, 11, 12);
+//PpBluetooth BtDevice(9600, 11, 12);
+SoftwareSerial BtDevice(12,11);
 
 void setup() {
   //Flashing diode - Initialization
@@ -43,19 +47,20 @@ void setup() {
   lcd.begin(16, 2); //inicjalizacja wyświetlacza o liczbie zanków 16 i 2 iniach
   lcd.home();// to to samo co lcd.setCursor(0, 0)
   lcd.cursor();
-  //LCD - Initialization displayed data
-  initDisplayStrings();
 
   //Mini servo 9g - Initialization
   miniServo9g.attach(servoPin);
   miniServo9g.write(iServoPos);
 
   //Serial monitor - Initialization
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(1000);
   //Bluetooth - Initalization
-  BtDevice.ble_help();
+  BtDevice.begin(9600);
+ //BtDevice.ble_help();
   delay(1000); 
+
+  Serial.print("Initialization done \n");
 
 }
 
@@ -64,13 +69,52 @@ void loop() {
   digitalWrite(13, HIGH);
   delay(iCycleTime);
 
-  readAnalogData();
-  //writeSerialData();
-  changeDisplayData();
-  displayData();
 
   //Prit serial message
-  Serial.print("Serial 115200 baud OK \n");
+  if (Serial.available() > 0) {
+    // Odczytanie pojedynczego znaku
+    char receivedChar = Serial.read();
+
+    // Wyświetlenie odczytanego znaku w monitorze szeregowym
+    Serial.print("Odczytano:\n");
+    Serial.println(receivedChar);
+
+    if(receivedChar = "1"){
+      Serial.print("Wysłano dane\n");
+      BtDevice.write("AT");
+
+      delay(100);
+      int iAvailable = BtDevice.available();
+      Serial.print("W buforze znajduje się : ");
+      Serial.println(iAvailable);      
+      Serial.print(" znaków.\n");
+
+    }
+  }
+
+  // Odczytaj wszystkie dostępne znaki
+  while (BtDevice.available()) {
+
+    int iAvailable = BtDevice.available();
+    Serial.print("W buforze znajduje się : ");
+    Serial.println(iAvailable);      
+    Serial.print(" znaków.\n");
+
+    char receivedChar = BtDevice.read();
+
+    if (isdigit(receivedChar)) {
+      int receivedInt = atoi(&receivedChar);
+      Serial.print("Odczytano jako int: ");
+      Serial.println(receivedInt);
+    } else {
+      int asciiValue = int(receivedChar);
+      Serial.print("Odczytano jako wartość ASCII: ");
+      Serial.println(asciiValue);      
+    }
+  
+    Serial.print("\n\n");
+
+  }
 
 
   digitalWrite(13, LOW);
@@ -79,140 +123,5 @@ void loop() {
 }
 
 //Funkcje
-void readAnalogData(){
-  //read joystick data
-  x_data = analogRead(x_pin) - x_data_raw;
-  y_data = analogRead(y_pin) - y_data_raw;
-  sw_data = digitalRead(sw_pin);}
-void initDisplayStrings() {
-  for (int i = 0; i <= 9; ++i) {
-    for (int j = 0; j <= 9; ++j) {
-      sDisplayLines[i][j] = String(char('A' + i)) + " " + String(char('A' + j));
-    }
-  }}
-void changeDisplayData(){
-  // change display data
-  if (x_data > 300) {
-    if (!bSwitchMemoX) {
-      //X data
-      if (iCursor == 4){
-        iDisplayX--;
-      }
-        //Y data
-     if (iCursor == 5){
-        iDisplayY--;
-      }
-    }
-    bSwitchMemoX = true;
-  } else if (x_data < -300) {
-    if (!bSwitchMemoX) {
-      //X data
-      if (iCursor == 4){
-        iDisplayX++;
-      }
-       //Y data
-      if (iCursor == 5){
-        iDisplayY++;
-      }
-    }
-    bSwitchMemoX = true;
-  } else {
-    bSwitchMemoX = false;
-  }
- 
-  if (y_data > 300) {
-    if (!bSwitchMemoY) {
-      iCursor++;
-    }
-    bSwitchMemoY = true;
-  } else if (y_data < -300) {
-    if (!bSwitchMemoY) {
-      iCursor--;
-   }
-    bSwitchMemoY = true;
-  } else {
-    bSwitchMemoY = false;
-  }
 
-  // iDisplayX range {1:9}, not 10 becouse 2 lines on display {9,10}
-  if (iDisplayX < 1) {
-    iDisplayX = 1;
-  } else if (iDisplayX > 9) {
-    iDisplayX = 9;
-  }
-  if (iDisplayY < 1) {
-    iDisplayY = 1;
-  } else if (iDisplayY > 10) {
-    iDisplayY = 10;
-  }}
-void displayData(){
 
-  lcd.home();// to to samo co lcd.setCursor(0, 0)
-  lcd.clear();
-  lcd.print("XY: ");
-  lcd.print(iDisplayX);
-  lcd.print(iDisplayY);
-  lcd.print(" Data: ");
-  lcd.print(sDisplayLines[iDisplayX - 1][iDisplayY - 1]);
-  lcd.setCursor(0, 1);
-  lcd.print("XY: ");
-  lcd.print(iDisplayX + 1);
-  lcd.print(iDisplayY);
-  lcd.print(" Data: ");
-  lcd.print(sDisplayLines[iDisplayX][iDisplayY - 1]);
-  /*
-  lcd.print("x ");//kursor porusza się z tekstem
-  lcd.print(x_data);//kursor porusza się z tekstem
-  lcd.print(" y ");//kursor porusza się z tekstem
-  lcd.print(y_data);//kursor porusza się z tekstem
-  lcd.setCursor(0, 1);
-  lcd.print("sw ");//kursor porusza się z tekstem
-  lcd.print(sw_data);//kursor porusza się z tekstem
-  */
-
-  lcd.setCursor(iCursor, 0);
-
-  //Serial.print(digitalRead(sw_pin));
-  }
-void writeSerialData(){
-  Serial.print("Switch:  ");
-  Serial.print(digitalRead(sw_pin));
-  Serial.print(" | ");
-  Serial.print("X-axis: ");
-  Serial.print(x_data);
-  Serial.print(" | ");
-  Serial.print("Y-axis: ");
-  Serial.print(y_data);
-  Serial.println(" | ");
-  Serial.print("Servo pos:  ");
-  Serial.print(iServoPos);
-  }
-void servoControl(){
-  /*
-    if (iServoPos >= 180){
-      bServoDir  = false;
-    } else if (iServoPos <= -180) {
-      bServoDir  = true;
-    }
-
-    if (bServoDir){
-      iServoPos += 10;
-    } else if (!bServoDir) {
-      iServoPos -= 10;
-    }
-  */
-
-  iServoPos = map(x_data, -500, 500, 0, 180);
-  miniServo9g.write(iServoPos);
-  /*
-  if (Serial.available() > 0) {
-    // Jeśli dostępne są dane w Serial Monitor, odczytaj je
-    iServoPos = Serial.parseInt();
-
-    // Wyświetl odczytaną wartość w Serial Monitor
-    Serial.print("Odczytano wartość: ");
-    Serial.println(iServoPos);
-    myServo.write(iServoPos);
-  }
-  */
-  }
